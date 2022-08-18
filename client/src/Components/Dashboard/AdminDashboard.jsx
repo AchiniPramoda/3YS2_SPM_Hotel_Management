caccff49da67eafe8f4afa7f62e1ca0f6fa90f9e
Shalitha Viraj <86163983+ShalithaViraj@users.noreply.github.com>
2022-08-18 21:41:51 +0530

    navbar and dashboard
    implement navbar and dashboard

--- client/src/Components/Dashboard/AdminDashboard.jsx
+++ client/src/Components/Dashboard/AdminDashboard.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import './AdminDashboard.css';
+import Navbar from "../Navbar/Navbar";
 
 function AdminDashBoard() {
     return (
-        <div className="header">
-            <h1>AdminDashBoard</h1>
-        </div>
+        <div>
+            
+            <Navbar />
+
+            
+            
+              <div class="card border-success mb-" className="card">
+                  <div class="card-header">Header</div>
+                    <div class="card-body">
+                     <h5 class="card-title">Primary card title</h5>
+                       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+               </div>
+
+
+            </div>
+           
     );
 } 
 
