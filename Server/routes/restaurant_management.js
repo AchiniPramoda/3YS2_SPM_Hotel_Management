890aaeaedea9def77b181588e2210ea18efbfb72
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-09-01 23:37:53 +0530

    CREATE restaurant Implementation

--- Server/routes/restaurant_management.js
+++ Server/routes/restaurant_management.js
@@ -18,3 +18,43 @@ router.get("/:id", async (req, res) => {
         .catch(err => res.status(400).send("Error : " + err))
 
 });
+
+/*Router for create Restaurant*/
+router.post('/AddRestaurant', async (req, res) => {
+
+    const restaurantName = req.body.restaurantName;
+    let restaurantExist = await Restaurant.findOne({
+        restaurantName: restaurantName,
+    });
+
+    if (restaurantExist)
+        return res.status(404).send("Restaurant already created!");
+
+
+    let image = req.files.photo;
+
+    let urlPrefix = "http://localhost:8080/static/images";
+    let imageName = Date.now() + "-" + image.name;
+
+    image.mv("./public/images/Restaurants/" + imageName, (err, result) => {
+        if (err) return res.status(400).send("Error : " + err);
+    });
+
+    let imageURL = urlPrefix + "/Restaurants/" + imageName;
+
+    let restaurant = new Restaurant({
+        restaurantName: req.body.restaurantName,
+        description: req.body.description,
+        other: req.body.other,
+        imageURL
+    });
+
+    try {
+        await restaurant.save();
+        res.send(restaurant);
+    } catch (err) {
+        console.log(err);
+        res.status(400).send('Error: ' + err);
+    }
+
+});
