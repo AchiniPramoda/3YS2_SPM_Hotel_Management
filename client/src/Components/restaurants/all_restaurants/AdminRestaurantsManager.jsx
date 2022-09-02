5c6af67d5f7c7bc40a87acf32c2d983c2b6ed8ea
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-09-02 15:01:36 +0530

    Update AdminRestaurantsManager.jsx

---
+++ client/src/Components/restaurants/all_restaurants/AdminRestaurantsManager.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
+import CreateRestaurant from "../create_restaurant/CreateRestaurant";
+import UpdateRestaurant from '../update_restaurant/UpdateRestaurant';
+import AllRestaurantsContainer from './restaurant_views/admin/AllRestaurantsContainer';
+
+const AdminRestaurantsManager = () => {
+    return (<React.Fragment>
+        <Router>
+            <Switch>
+                <Route
+                    path="/admin/restaurants/EditRestaurant/:id"
+                    component={(props) => (
+                        <UpdateRestaurant {...props} key={window.location.pathname} />
+                    )}
+                />
+                <Route path="/admin/restaurants/CreateRestaurant">
+                    <CreateRestaurant />
+                </Route>
+                <Route path="/">
+                    <AllRestaurantsContainer />
+                </Route>
+            </Switch>
+        </Router>
+    </React.Fragment>);
+}
+
+export default AdminRestaurantsManager;
