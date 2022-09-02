652d651641a0e155963876c1c903efe6d83c289a
nethmini-11 <83303587+nethmini-11@users.noreply.github.com>
2022-09-02 15:04:23 +0530

    Update AllRestaurantsForUser.jsx

---
+++ client/src/Components/restaurants/all_restaurants/restaurant_views/user/AllRestaurantsForUser.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import axios from "axios"
+
+import RestaurantSmallView from './RestaurantSmallView';
+
+class AllRestaurantsForUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            restaurants: []
+        }
+    }
+
+    // Get all restaurants from datasbase
+    componentDidMount() {
+        axios.get("http://localhost:8080/api/restaurants/").then(res => {
+            this.setState({ restaurants: res.data });
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    render() {
+        return (
+
+            <div className="container mt-5">
+                <div className="row">
+                    <h3 className={"text-secondary text-center mb-5"}>All Restaurants</h3>
+                    <React.Fragment>
+                        {
+                            this.state.restaurants.map(res => {
+                                return <RestaurantSmallView restaurant={res} count={4} />
+                            })
+                        }
+                    </React.Fragment>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default AllRestaurantsForUser;
